f39f39e5746da182f38754cf011f165d20836e98
Nadia18ti <50732801+Nadia18ti@users.noreply.github.com>
2022-02-18 01:51:12 +0700

    Create addmatpel.php

---
+++ addmatpel.php
@@ -0,0 +1,57 @@
+<?php include 'header.php'; ?>
+<?php include 'sidebar.php'; ?>
+
+
+<main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Form Validation</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item">Forms</li>
+                <li class="breadcrumb-item active">Validation</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Tambah Mata Pelajaran</h5>
+
+                        <!-- Browser Default Validation -->
+                        <form class="row" method="post" enctype="multipart/form-data">
+                            <div class="form-group mb-3">
+                                <label for="validationDefault01" class="form-label">ID</label>
+                                <input type="text" class="form-control" name="id" id="validationDefault01" placeholder="123xxxx" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="validationDefault03" class="form-label">Nama Mata Pelajaran</label>
+                                <input type="text" class="form-control" name="nama_mapel" id="validationDefault03" placeholder="nama matapelajaran" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <button class="btn btn-primary" name="save" type="submit">Tambah Data</button>
+                            </div>
+                        </form>
+                        <?php
+
+                        include 'koneksi.php';
+
+                        if (isset($_POST['save'])) {
+                            $db->query("INSERT INTO mapel(id,nama_mapel)
+                                            Values('$_POST[id]','$_POST[nama_mapel]')");
+
+                            echo "<script>alert('Siswa Berhasil Ditambahkan');window.location.href='matpel.php';1</script>";
+                        }
+                        ?>
+                        <!-- End Browser Default Validation -->
+
+                    </div>
+                </div>
+
+            </div>
+            <?php include 'footer.php'; ?>
