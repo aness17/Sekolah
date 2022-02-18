588ecf9cd4af991799c29a7439404c482e6e1057
aness17 <52444655+aness17@users.noreply.github.com>
2022-02-18 11:43:43 +0700

    aksi

--- addnilai.php
+++ addnilai.php
@@ -26,28 +26,56 @@
                         <!-- Browser Default Validation -->
                         <form class="row" method="post" enctype="multipart/form-data">
                             <div class="form-group mb-3">
-                                <label for="validationDefault01" class="form-label">nis</label>
-                                <input type="text" class="form-control" name="nis" id="validationDefault01" placeholder="123xxxx" required>
+                                <label for="validationDefault01" class="form-label">Nama</label>
+                                <?php
+                                include 'koneksi.php'; ?>
+
+                                <!-- Browser Default Validation -->
+
+                                <select name="nis" id="nis" class="form-control">
+
+                                    <?php
+                                    $sql = "SELECT * FROM siswa";
+
+                                    $query = mysqli_query($db, $sql);
+
+                                    while ($m = mysqli_fetch_array($query, MYSQLI_BOTH)) {
+                                    ?>
+
+                                        <option value="<?= $m['nis'] ?>"><?= $m['nama'] ?></option>
+                                    <?php } ?>
+                                </select>
                             </div>
                             <div class="form-group mb-3">
                                 <label for="validationDefault03" class="form-label">Nilai ulangan</label>
-                                <input type="text" class="form-control" name="nilai_ulangan" id="validationDefault03" placeholder="nilai ulangan" required>
+                                <input type="number" class="form-control" name="nilai_ulangan" id="validationDefault03" placeholder="nilai ulangan" required>
                             </div>
                             <div class="form-group mb-3">
                                 <label for="validationDefault03" class="form-label">Nilai UTS</label>
-                                <input type="text" class="form-control" name="nilai_uts" id="validationDefault03" placeholder="nilai uts" required>
+                                <input type="number" class="form-control" name="nilai_uts" id="validationDefault03" placeholder="nilai uts" required>
                             </div>
                             <div class="form-group mb-3">
                                 <label for="validationDefault03" class="form-label">Nilai UAS</label>
-                                <input type="text" class="form-control" name="nilai_uas" id="validationDefault03" placeholder="nilai uas" required>
+                                <input type="number" class="form-control" name="nilai_uas" id="validationDefault03" placeholder="nilai uas" required>
                             </div>
                             <div class="form-group mb-3">
-                                <label for="validationDefault03" class="form-label">Nilai Akhir</label>
-                                <input type="text" class="form-control" name="nilai_akhir" id="validationDefault03" placeholder="nilai akhir" required>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="validationDefault03" class="form-label">ID Guru Matapelajaran</label>
-                                <input type="text" class="form-control" name="nilai_guru_mapel" id="validationDefault03" placeholder="id guru matapelajaran" required>
+                                <label for="validationDefault03" class="form-label">Nama Guru Matapelajaran</label>
+
+                                <!-- Browser Default Validation -->
+
+                                <select name="id_guru_mapel" id="id_guru_mapel" class="form-control">
+
+                                    <?php
+                                    $sql = "SELECT guru_mapel.id as id, guru.nama as nama FROM guru_mapel JOIN guru ON guru_mapel.nip= guru.nip";
+
+                                    $query = mysqli_query($db, $sql);
+
+                                    while ($m = mysqli_fetch_array($query, MYSQLI_BOTH)) {
+                                    ?>
+
+                                        <option value="<?= $m['id'] ?>"><?= $m['nama'] ?></option>
+                                    <?php } ?>
+                                </select>
                             </div>
 
 
@@ -56,13 +84,15 @@
                             </div>
                         </form>
                         <?php
-
-                        include 'koneksi.php';
-
+                        // (nilai_ulangan = 20%, nilai uts =30%, dan nilai uas = 50%)
                         if (isset($_POST['save'])) {
-                            $db->query("INSERT INTO nilai(nis,nilai_ulangan,nilai_uts,nilai_uas,nilai_akhir,nilai_guru_mapel)
-                                            Values('$_POST[nis]','$_POST[nilai_ulangan]','$_POST[nilai_uts]','$_POST[nilai_uas]','$_POST[nilai_akhir]','$_POST[id_guru_mapel]')");
-
+                            $nilai_akhir = (0.2 * $_POST['nilai_ulangan']) + (0.3 * $_POST['nilai_uts']) + (0.5 * $_POST['nilai_uas']);
+                            $sql = "INSERT INTO nilai(nis,nilai_ulangan,nilai_uts,nilai_uas,nilai_akhir,id_guru_mapel)
+                                            Values('$_POST[nis]','$_POST[nilai_ulangan]','$_POST[nilai_uts]',
+                                            '$_POST[nilai_uas]','$nilai_akhir','$_POST[id_guru_mapel]')";
+                            $db->query($sql);
+                            // echo $nilai_akhir;
+                            // die;
                             echo "<script>alert('Nilai Berhasil Ditambahkan');window.location.href='nilai.php';1</script>";
                         }
                         ?>
